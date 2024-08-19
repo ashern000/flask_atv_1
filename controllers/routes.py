cf1efeafe80a5f234172178c3b44809122a71009
Asher Novelli | Plano Angelus <asher.novelli@planoangelus.com.br>
2024-08-19 17:57:11 -0300

    Add new style for pages

--- controllers/routes.py
+++ controllers/routes.py
@@ -8,14 +8,14 @@ def init_app(app):
     def home():
         return render_template('index.html')
 
-    @app.route('/sistemas_operacionais', methods=['GET', 'POST'])
-    def games():
+    @app.route('/produtos', methods=['GET', 'POST'])
+    def produtos():
         game = productsIt[0]
         
         if request.method == 'POST':
             if request.form.get('jogador'):
                 jogadores.append(request.form.get('jogador'))
-        return render_template('games.html',
+        return render_template('produtos.html',
                                 game=game,
                                 jogadores=jogadores)
         
@@ -23,7 +23,7 @@ def init_app(app):
     def products_cad():
         if request.method == 'POST':
             if request.form.get('nome') and request.form.get('ano') and request.form.get('categoria'):
-                productsIt.append({'Título' : request.form.get('titulo'), 'Ano' : request.form.get('ano'), 'Categoria' : request.form.get('categoria')})
+                productsIt.append({'Nome' : request.form.get('nome'), 'Ano' : request.form.get('ano'), 'Categoria' : request.form.get('categoria')})
         
-        return render_template('cadgames.html',
+        return render_template('cadastro_produtos.html',
                                 productsIt=productsIt)
